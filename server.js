bd746afb7dea95653edb7906dc2a77942359b30b
Raja R <rraja9513@gmail.com>
2021-06-08 16:51:34 +0530

    08-06-2021 uploadaccess

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 const app=express();
 app.use(cors());
 app.use(express.json());
+app.use('/uploads',express.static('uploads'));
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
